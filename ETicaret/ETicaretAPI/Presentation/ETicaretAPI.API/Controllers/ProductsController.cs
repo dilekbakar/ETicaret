803d86d92d11fdcc3ca0321219ea33cbf5c35531
Dilek BAKAR <108486801+dilekbakar@users.noreply.github.com>
2023-03-28 16:27:30 +0300

    commit
    first commit

---
+++ ETicaret/ETicaretAPI/Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using ETicaretAPI.Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IProductReadRepository _productReadRepository;
+
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+        }
+
+        public async void Get()
+        {
+
+        }
+    }
+}
